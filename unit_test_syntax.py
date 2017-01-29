d1b2e370c6cef25a522f6e6a3b21d8c9472222ed
MaxOvcharov <ovcharovmax@yandex.ru>
2017-01-29 20:23:11 +0300

    Added all unittest methods

--- unit_test_syntax.py
+++ unit_test_syntax.py
@@ -19,3 +19,40 @@ class TestUnittestMethods(unittest.TestCase):
             Вызывает ошибку, если значение аргумента != True
         """
         self.failUnless(True)
+
+    def testFailIf(self):
+        """Устаревшая функция, теперь называется assertFalse()"""
+        self.assertFalse(False)
+
+    def testAssertFalse(self):
+        """Вызывает ошибку, если начение аргумента != False"""
+        self.assertFalse(False)
+
+    def testEqual(self):
+        """Вызывает ошибку, если значения двух аргументов НЕ равны"""
+        self.failUnlessEqual(1, 3 - 2)
+
+    def testNotEqual(self):
+        """Вызывает ошибку, если значения двух аргументов равны"""
+        self.failIfEqual(2, 3 - 2)
+
+    def testNotAlmostEqual(self):
+        """
+            Старое название функции.
+            Теперь называется assertNotAlmostEqual().
+            Сравниевает два аргумента с округлением, можно задать
+            это округление. Вызывает ошибку, если занчения равны.
+        """
+        self.failIfAlmostEqual(1.1, 3.5 - 2.0, places=1)
+
+    def testAlmostEqual(self):
+        """
+            Старое название функции.
+            Теперь называется assertAlmostEquals().
+            Сравнивается два аргумента с округление, можно задать
+            это округление. Вызывает ошибку, если значения НЕ равны
+        """
+        self.failUnlessAlmostEqual(1.1, 3.3 - 2.0, places=0)
+
+if __name__ == '__main__':
+    unittest.main()
